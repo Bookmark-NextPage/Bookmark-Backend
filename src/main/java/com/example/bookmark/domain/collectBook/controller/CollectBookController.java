23f62100108cff07af8b6874496f8763f2ede110
slwnt31 <slwnt8531@gmail.com>
2026-08-10 19:44:19 +0900

    style: import 문 정리 (#11)

--- src/main/java/com/example/bookmark/domain/collectBook/controller/CollectBookController.java
+++ src/main/java/com/example/bookmark/domain/collectBook/controller/CollectBookController.java
@@ -9,7 +9,6 @@ import com.example.bookmark.domain.collectBook.dto.response.CollectBookListRespo
 import com.example.bookmark.domain.collectBook.service.CollectBookService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
